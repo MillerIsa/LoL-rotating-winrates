d505caa4804194f364ad2d0cb99c43b3f2af02ad
Isaiah Miller <miller.isaiah.n@gmail.com>
2017-03-24 14:58:40 -0700

    add reddit formater

---
+++ src/windatasort/reddit_runner.py
@@ -0,0 +1,15 @@
+'''
+Created on Mar 18, 2017
+
+@author: Brian-VAIO
+'''
+from sheets import PrintToReddit
+
+def main():
+    redditPrinter=PrintToReddit()
+    redditPrinter.updateTable2(spreadsheatId="1hbD9UsOaR5SSy6BrD4BzE0sHy5R9GttX0v1wfaqLnMY",subSheetName="winRates")
+
+
+
+if __name__ == "__main__":
+    main()
